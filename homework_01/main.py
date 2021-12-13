5194e3e0dd8d4e8797dfa3f5a256fdffd8618246
kkartashov <kartashov256@gmail.com>
2021-12-13 17:32:36 +0300

    functions updated to return requested by task value

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,15 @@
 """
 
 
-def power_numbers():
+def power_numbers(*items):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    
+    return [ item*item for item in items ]
 
 
 # filter types
@@ -19,7 +21,26 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(number):
+    """
+    функция, которая проверяет, является ли число простым.
+    можно сделать декоратор, который собирает простые числа,
+    который постепенно складывает простые числа в кэш, после
+    чего каждый следующий запуск уже сравнивает с числами
+    из кэша, а затем уже на следующие числа.
+    Более экономный вариант для списка: найти самое большое число, 
+    вызвать рекурсивную функцию, которая найдёт все простые числа до него,
+    затем проверить, входят ли другие элементы списка в простые числа
+    
+    Для единичного числа можно 
+    """
+    for i in range(2,number):
+        if number % i == 0:
+            return False
+    return True    
+
+
+def filter_numbers(nums, type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +51,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if type == EVEN:
+        return list(filter(lambda num: num % 2 == 0, nums))    
+    if type == ODD:
+        return list(filter(lambda num: num % 2 != 0, nums))
+    if type == PRIME:
+        return list(filter(is_prime, nums))
